06734c3b09257e32ee414532029925cc0331fdf5
Andi Berisha <aberisha@qa.com>
2021-02-16 16:51:24 +0000

    removed old project

--- ToDo_Project/src/main/resources/schema-test.sql
+++
@@ -1,5 +0,0 @@
-drop table if exists task_domain CASCADE ;
-drop table if exists user_domain CASCADE ;
-create table task_domain (id bigint auto_increment not null, has_task_been_completed_check boolean not null, task_name varchar(255) not null, user_id bigint, primary key (id));
-create table user_domain (id bigint auto_increment not null, user_name varchar(255), user_surname varchar(255), primary key (id));
-alter table task_domain add constraint FKj7odm2792jkgbne8tea5jd8iu foreign key (user_id) references user_domain on delete cascade;
